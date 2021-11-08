5f7ca6374bb08a421f323aad58038732bfb09f81
Paloma <paloma.arevalog@gmail.com>
2021-11-08 11:39:00 +0100

    change template

--- src/components/App.js
+++
@@ -1,9 +0,0 @@
-// Fichero src/components/App.js
-import '../styles/App.scss';
-//import PropTypes from 'prop-types'; las importamos en el componente en que queramos usarlas
-
-const App = () => {
-	return <div>Hola mundo</div>;
-};
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-// Fichero src/index.js (código nuevo)
-
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter } from 'react-router-dom';
-import App from './components/App';
+import './index.scss';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(
-	<HashRouter>
-		<App />
-	</HashRouter>,
-	document.getElementById('root')
-);
+/* GLOBAL VARIABLES */
+
+window.$primaryLanguage = 'en';
+window.$secondaryLanguage = 'pl';
+window.$primaryLanguageIconId = 'primary-lang-icon';
+window.$secondaryLanguageIconId = 'secondary-lang-icon';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+serviceWorker.register();
