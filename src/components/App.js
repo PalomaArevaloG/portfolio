f56024ff345ded7f7609253b0568b98bbb18ffc3
Paloma <paloma.arevalog@gmail.com>
2021-11-08 10:34:51 +0100

    add structure

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+// Fichero src/components/App.js
+import '../styles/App.scss';
+//import PropTypes from 'prop-types'; las importamos en el componente en que queramos usarlas
+
+const App = () => {
+	return <div>Hola mundo</div>;
+};
+
+export default App;
