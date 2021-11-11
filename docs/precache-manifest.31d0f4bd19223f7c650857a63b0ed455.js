d508db1d3069fca40d805edb50b7b58f3724ff55
Paloma <paloma.arevalog@gmail.com>
2021-11-11 17:19:14 +0100

    Publish in GitHub Pages

---
+++ docs/precache-manifest.31d0f4bd19223f7c650857a63b0ed455.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "436b026863f5dac8d841e01d10e7c29c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8745fc791629b524543c",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "a9c63325bc08d5fe4433",
+    "url": "/static/css/main.7220ddaa.chunk.css"
+  },
+  {
+    "revision": "8745fc791629b524543c",
+    "url": "/static/js/2.245488ee.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.245488ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9c63325bc08d5fe4433",
+    "url": "/static/js/main.53ef2a38.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
