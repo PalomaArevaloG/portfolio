f08cf5119fea3fa5ca8e4792b5c073f79213d236
Paloma <paloma.arevalog@gmail.com>
2021-11-16 20:25:00 +0100

    Publish in GitHub Pages

---
+++ docs/precache-manifest.015e002f83ce537565e69bc83dd8987b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5928d87a17693f9e0a41f53d365f35f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "faf5b283cd52b8c7fc30",
+    "url": "./static/css/2.1a1b35f8.chunk.css"
+  },
+  {
+    "revision": "cfd0db999f869c83db2e",
+    "url": "./static/css/main.4f22d06a.chunk.css"
+  },
+  {
+    "revision": "faf5b283cd52b8c7fc30",
+    "url": "./static/js/2.16c97489.chunk.js"
+  },
+  {
+    "revision": "efc0bc89bcf702a3aa1127d8ad15fc2e",
+    "url": "./static/js/2.16c97489.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfd0db999f869c83db2e",
+    "url": "./static/js/main.e7bee2d1.chunk.js"
+  },
+  {
+    "revision": "610a38b7b5a7cb7b2d5e",
+    "url": "./static/js/runtime-main.916853f0.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.d038488f4906f0e186e98ded5dca693b.js"
+  "./precache-manifest.015e002f83ce537565e69bc83dd8987b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
